f74d2d0fab4806a179e758abbfb6eb25f89c2f78
Evan Miller <miller.evan815@gmail.com>
2015-05-23 20:13:38 -0400

    Bringing remote up to date with my old laptop - last commit from this beast?

--- src/io/github/agentepsilon/ExcelGrabber.java
+++ src/io/github/agentepsilon/ExcelGrabber.java
@@ -6,6 +6,7 @@ import org.apache.poi.ss.usermodel.*;
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * Created by Evan on 1/19/2015.
@@ -18,21 +19,27 @@ public class ExcelGrabber {
             Workbook wb = WorkbookFactory.create(fc.getSelectedFile());
             Sheet s = wb.getSheetAt(0);
             for(Row r : s){
-                Cell name = null, id = null, div = null;
+                ArrayList<Cell> arrayList = new ArrayList<Cell>(3);
                 for(Cell c : r) {
                     switch(c.getColumnIndex()){
-                        case 0: name = c;
+                        case 0: arrayList.add(0,c);
                             break;
-                        case 1: id = c;
+                        case 1: arrayList.add(1,c);
                             break;
-                        case 2: div = c;
+                        case 2: arrayList.add(2,c);
                             break;
                         default: break;
                     }
                 }
-                String idnum = ((int) id.getNumericCellValue())+"";
-                while(idnum.length()<5){idnum="0"+idnum;}
-                ScantronPrinter.printScantron(new ScantronPrinter(name.getStringCellValue(), idnum, div.getStringCellValue(), false));
+                if(arrayList.size()==3) {
+                    String idnum = ((int) arrayList.get(2).getNumericCellValue()) + "";
+                    while (idnum.length() < 5) {
+                        idnum = "0" + idnum;
+                    }
+                    ScantronPrinter.printScantron(new ScantronPrinter(arrayList.get(0).getStringCellValue(), idnum, arrayList.get(1).getStringCellValue(), false));
+                }else{
+                    JOptionPane.showMessageDialog(null, "Row " + r.getRowNum() + " has incorrect number of cells!\nRemember, its [Name], [Division], [ID].");
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
